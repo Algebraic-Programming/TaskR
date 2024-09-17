7092f3b36b023dbc24eed98f786f5ba040fe7b03
Sergio Martin <sergio.miguel.martin@huawei.com>
2024-09-17 10:57:55 +0200

    Improving worker specific example to change affinity on the fly

--- examples/local/workerSpecific/source/workerSpecific.hpp
+++ examples/local/workerSpecific/source/workerSpecific.hpp
@@ -6,15 +6,36 @@
 
 void workFc()
 {
-  auto taskLabel    = taskr::getCurrentTask()->getLabel();
+  auto currentTask = taskr::getCurrentTask();
+  auto taskLabel    = currentTask->getLabel();
   int  currentCPUId = sched_getcpu();
 
-  printf("Task %lu running on CPU %d\n", taskLabel, currentCPUId);
+  ////// First launched on even cpus
+
+  printf("Task %lu first run running on CPU %d\n", taskLabel, currentCPUId);
 
   // Sanity check
   if ((int)taskLabel != currentCPUId)
   {
     fprintf(stderr, "Task label (%lu) does not coincide with the current CPU id! (%d)\n", taskLabel, currentCPUId);
+    std::abort();
+  }
+
+  // Changing to odd cpus
+  currentTask->setWorkerAffinity(currentTask->getWorkerAffinity() + 1);
+
+  // Suspending
+  currentTask->suspend();
+
+  ///// Now launched in odd cpus
+
+  currentCPUId = sched_getcpu();
+  printf("Task %lu second run running on CPU %d\n", taskLabel, currentCPUId);
+
+  // Sanity check
+  if ((int)taskLabel + 1 != currentCPUId)
+  {
+    fprintf(stderr, "Task label (%lu) + 1 does not coincide with the current CPU id! (%d)\n", taskLabel, currentCPUId);
     std::abort();
   }
 }
@@ -24,6 +45,9 @@ void workerSpecific(taskr::Runtime &taskr, const size_t workerCount)
   // Setting callback to free a task as soon as it finishes executing
   taskr.setCallbackHandler(HiCR::tasking::Task::callback_t::onTaskFinish, [](taskr::Task *task) { delete task; });
 
+  // Auto-adding task when it suspends.
+  taskr.setCallbackHandler(HiCR::tasking::Task::callback_t::onTaskSuspend, [&](taskr::Task *task) { taskr.resumeTask(task); });
+  
   // Creating the execution units (functions that the tasks will run)
   auto workTaskfc = HiCR::backend::host::L1::ComputeManager::createExecutionUnit([]() { workFc(); });
 
