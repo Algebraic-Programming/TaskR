97ddcc97433295c18802e52fbc14058ef670a511
Luca Terracciano <luca.terraciano@huawei.com>
2024-09-18 17:54:53 +0200

    add ompss version of cholesky

---
+++ examples/local/cholesky/ompss/cholesky.hpp
@@ -0,0 +1,166 @@
+#include <cstdio>
+#include <chrono>
+#include <lapack.h>
+#include <cblas.h>
+#include <hicr/backends/host/pthreads/L1/communicationManager.hpp>
+#include <hicr/backends/host/hwloc/L1/memoryManager.hpp>
+
+#include "../utils.hpp"
+#include "init.hpp"
+#include "verify.hpp"
+
+extern uint8_t *_dependencyGrid;
+extern uint64_t _blocks;
+
+__INLINE__ void potrf(double *A, const uint32_t blockSize, const uint32_t matrixDimensionSize)
+{
+  char UPLO = 'L';
+  int  info = 0;
+  int  bs   = blockSize;
+  int  ms   = matrixDimensionSize;
+  LAPACK_dpotrf2(&UPLO, &bs, A, &ms, &info);
+  if (info != 0) HICR_THROW_RUNTIME("dpotrf2 failed: %d", info);
+}
+
+__INLINE__ void trsm(double *A, double *B, const uint32_t blockSize, const uint32_t matrixDimensionSize)
+{
+  cblas_dtrsm(CblasRowMajor, CblasLeft, CblasUpper, CblasTrans, CblasNonUnit, blockSize, blockSize, 1.0, A, matrixDimensionSize, B, matrixDimensionSize);
+}
+
+__INLINE__ void gemm(double *A, double *B, double *C, const uint32_t blockSize, const uint32_t matrixDimensionSize)
+{
+  cblas_dgemm(CblasRowMajor, CblasTrans, CblasNoTrans, blockSize, blockSize, blockSize, -1.0, A, matrixDimensionSize, B, matrixDimensionSize, 1.0, C, matrixDimensionSize);
+}
+
+__INLINE__ void syrk(double *A, double *B, const uint32_t blockSize, const uint32_t matrixDimensionSize)
+{
+  cblas_dsyrk(CblasRowMajor, CblasUpper, CblasTrans, blockSize, blockSize, -1.0, A, matrixDimensionSize, 1.0, B, matrixDimensionSize);
+}
+
+void cholesky(const std::vector<std::vector<std::shared_ptr<HiCR::L0::LocalMemorySlot>>> &blockMatrix, const uint32_t blocks, const uint32_t blockSize)
+{
+  for (uint32_t i = 0; i < blocks; i++)
+  {
+// Diagonal Block factorization
+#pragma oss task depend(inout : _dependencyGrid[i * _blocks + i]) label("cholesky")
+    potrf((double *)blockMatrix[i][i]->getPointer(), blockSize, blockSize);
+
+    // Triangular systems
+    for (uint32_t j = i + 1; j < blocks; j++)
+    {
+#pragma oss task depend(in : _dependencyGrid[i * _blocks + i]) depend(inout : _dependencyGrid[i * _blocks + j]) label("trsm")
+      trsm((double *)blockMatrix[i][i]->getPointer(), (double *)blockMatrix[i][j]->getPointer(), blockSize, blockSize);
+    }
+
+    // Update trailing matrix
+    for (uint32_t j = i + 1; j < blocks; j++)
+    {
+      for (uint32_t k = i + 1; k < j; k++)
+      {
+#pragma oss task depend(in : _dependencyGrid[i * _blocks + k]) depend(in : _dependencyGrid[i * _blocks + j]) depend(inout : _dependencyGrid[k * _blocks + j]) label("gemm")
+        gemm((double *)blockMatrix[i][k]->getPointer(), (double *)blockMatrix[i][j]->getPointer(), (double *)blockMatrix[k][j]->getPointer(), blockSize, blockSize);
+      }
+
+#pragma oss task depend(in : _dependencyGrid[i * _blocks + j]) depend(inout : _dependencyGrid[j * _blocks + j]) label("syrk")
+      syrk((double *)blockMatrix[i][j]->getPointer(), (double *)blockMatrix[j][j]->getPointer(), blockSize, blockSize);
+    }
+  }
+
+#pragma oss taskwait
+}
+
+void choleskyDriver(const uint32_t                                           matrixDimension,
+                    const uint32_t                                           blocks,
+                    const bool                                               readFromFile,
+                    const bool                                               checkResult,
+                    HiCR::backend::host::hwloc::L1::MemoryManager           *memoryManager,
+                    HiCR::backend::host::pthreads::L1::CommunicationManager *communicationManager,
+                    const std::shared_ptr<HiCR::L0::MemorySpace>            &memorySpace,
+                    const std::string                                       &matrixPath)
+{
+  // Compute the blocks for the block matrix
+  const uint32_t blockSize = matrixDimension / blocks;
+
+  // Compute matrix size
+  auto matrixSize = matrixDimension * matrixDimension * sizeof(double);
+
+  // Init global variables
+  auto dependencyGrid = memoryManager->allocateLocalMemorySlot(memorySpace, blocks * blocks * sizeof(uint8_t));
+  _dependencyGrid     = (uint8_t *)dependencyGrid->getPointer();
+  _blocks             = blocks;
+
+  // Allocate matrix
+  auto matrix = memoryManager->allocateLocalMemorySlot(memorySpace, matrixSize);
+
+  // Populate matrix from file
+  if (readFromFile == true)
+  {
+    auto inputPath = matrixPath + "/input/matrix-" + std::to_string(matrixDimension);
+    readMatrixFromFile((double *)matrix->getPointer(), matrixSize, inputPath);
+  }
+  else { initMatrix((double *)matrix->getPointer(), matrixDimension); }
+
+  // Allocate buffer to save the inital matrix if we need to check the result later
+  std::shared_ptr<HiCR::L0::LocalMemorySlot> originalMatrix;
+
+  if (checkResult == true)
+  {
+    // Allocate matrix
+    originalMatrix = memoryManager->allocateLocalMemorySlot(memorySpace, matrixSize);
+
+    // Read ground truth matrix from file
+    auto groundTruthPath = matrixPath + "/output/matrix-" + std::to_string(matrixDimension);
+    readMatrixFromFile((double *)originalMatrix->getPointer(), matrixSize, groundTruthPath);
+  }
+
+  // Initialize the block matrix
+  std::vector<std::vector<std::shared_ptr<HiCR::L0::LocalMemorySlot>>> blockMatrix(blocks, std::vector<std::shared_ptr<HiCR::L0::LocalMemorySlot>>(blocks));
+
+  // Malloc the block matrix memory regions
+  allocateBlockMatrix(blocks, blockSize, memoryManager, memorySpace, blockMatrix);
+
+  // Convert the original matrix (linear) to a block matrix. Each block has a size of blockSize * blockSize
+  convertToBlockMatrix((double *)matrix->getPointer(), matrixDimension, blockSize, blockMatrix);
+
+  printf("Start...\n");
+  // Run the task graph
+  auto start = std::chrono::high_resolution_clock::now();
+  cholesky(blockMatrix, blocks, blockSize);
+  auto end   = std::chrono::high_resolution_clock::now();
+  auto delta = std::chrono::duration<double>(end - start);
+
+  printf("Cholesky decomposition took %.4f seconds\n", delta.count());
+
+  // Exit if the check on the result is not required
+  if (checkResult == false)
+  {
+    freeBlockMatrix(blockMatrix, memoryManager);
+    memoryManager->freeLocalMemorySlot(matrix);
+    memoryManager->freeLocalMemorySlot(dependencyGrid);
+    return;
+  }
+
+  // Check the result of Cholesky decomposition
+  // Transform the matrix from the block format to a linear one
+  convertToPlainMatrix((double *)matrix->getPointer(), matrixDimension, blockSize, blockMatrix);
+
+  // Make the matrix lower triangular
+  for (uint32_t i = 0; i < matrixDimension; i++)
+  {
+    for (uint32_t j = i + 1; j < matrixDimension; j++)
+    {
+      ((double *)matrix->getPointer())[(j * matrixDimension) + i] = ((double *)matrix->getPointer())[(i * matrixDimension) + j];
+      ((double *)matrix->getPointer())[(i * matrixDimension) + j] = 0.0;
+    }
+  }
+
+  // Verify result
+  auto equal = areMatrixEqual((double *)originalMatrix->getPointer(), (double *)matrix->getPointer(), matrixDimension);
+  if (equal == false) { fprintf(stderr, "factorization failed. Actual matrix and ground truth are not equal\n"); }
+
+  // Free matrix memory
+  freeBlockMatrix(blockMatrix, memoryManager);
+  memoryManager->freeLocalMemorySlot(matrix);
+  memoryManager->freeLocalMemorySlot(originalMatrix);
+  memoryManager->freeLocalMemorySlot(dependencyGrid);
+}
---
+++ examples/local/cholesky/ompss/main.cpp
@@ -0,0 +1,57 @@
+#include <cstdio>
+#include <random>
+#include <hwloc.h>
+#include <chrono>
+#include <pthread.h>
+#include <hicr/backends/host/pthreads/L1/computeManager.hpp>
+#include <hicr/backends/host/pthreads/L1/communicationManager.hpp>
+#include <hicr/backends/host/hwloc/L1/memoryManager.hpp>
+#include <hicr/backends/host/hwloc/L1/topologyManager.hpp>
+
+#include "cholesky.hpp"
+
+uint8_t *_dependencyGrid = NULL;
+uint64_t _blocks         = 0;
+
+int main(int argc, char **argv)
+{
+  // Checking arguments
+  if (argc < 5)
+  {
+    fprintf(stderr, "Error: <matrix size> <blocks> <check result> <matrix path>\n");
+    exit(-1);
+  }
+
+  // Reading argument
+  uint32_t    matrixDimension = std::atoi(argv[1]);
+  uint32_t    blocks          = std::atoi(argv[2]);
+  bool        readFromFile    = std::atoi(argv[3]);
+  bool        checkResult     = std::atoi(argv[4]);
+  std::string matrixPath      = "";
+  if (argc == 6) { matrixPath = std::string(argv[5]); }
+
+  // Creating HWloc topology object
+  hwloc_topology_t topology;
+
+  // Reserving memory for hwloc
+  hwloc_topology_init(&topology);
+
+  // Initializing HWLoc-based host (CPU) topology and memory manager
+  HiCR::backend::host::hwloc::L1::TopologyManager         tm(&topology);
+  HiCR::backend::host::hwloc::L1::MemoryManager           memoryManager(&topology);
+  HiCR::backend::host::pthreads::L1::CommunicationManager communicationManager;
+
+  // Asking backend to check the available devices
+  const auto t = tm.queryTopology();
+
+  // Memory space to use
+  auto memorySpace = (*(*t.getDevices().begin())->getMemorySpaceList().begin());
+
+  // Running Cholesky factorization example
+  choleskyDriver(matrixDimension, blocks, readFromFile, checkResult, &memoryManager, &communicationManager, memorySpace, matrixPath);
+
+  // Freeing up memory
+  hwloc_topology_destroy(topology);
+
+  return 0;
+}
